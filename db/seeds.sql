b2fecd035512ce854ff5160224faff468c4c52c1
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-01-15 15:22:49 -0700

    setting up new file structure

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Portabello Burger');
+INSERT INTO burgers (burger_name) VALUES ('Basic Burger');
+
+SELECT * FROM burgers;
