f2baadfa7939895941dad6d2e3a12fbc5d293899
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-01-15 16:37:06 -0700

    updating handlebars and routing

--- controllers/sushi_controller.js
+++ controllers/sushi_controller.js
@@ -2,20 +2,31 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model to use its database functions.
-var sushi = require("../models/sushi.js");
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
- 
+ db.Eatsushi.findAll({}).then(function(dbSushi) {
+   res.json(dbSushi);
+ })
 });
 
-router.post("/api/burgers", function(req, res) {
- 
+router.post("/api/sushis", function(req, res) {
+ db.Eatsushi.create(req.body).then(function(dbSushi) {
+   res.json(dbSushi);
+ })
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  
+router.put("/api/sushis/:id", function(req, res) {
+  db.Eatsushi.update(
+    req.body,
+    {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbSushi) {
+      res.json(dbSushi);
+    })
 });
 
 // Export routes for server.js to use.
